d8ba1226497482938bb43ea4e34221cab6ca1254
Jan Piotrowski <piotrowski+github@gmail.com>
2024-06-05 11:37:19 +0000

    convert to use neon and @prisma/adapter-neon

--- lib/db.ts
+++ lib/db.ts
@@ -4,15 +4,23 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
+// import { Pool } from 'pg'
+// import { PrismaPg } from '@prisma/adapter-pg'
 
+// const connectionString = `${process.env.DATABASE_URL}`
+
+// const pool = new Pool({ connectionString })
+// const adapter = new PrismaPg(pool)
+
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+// import ws from 'ws'
+// neonConfig.webSocketConstructor = ws
 const connectionString = `${process.env.DATABASE_URL}`
 
 const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaNeon(pool)
 
-export const db = globalThis.prisma || new PrismaClient({ log: ['query']});
+export const db = globalThis.prisma || new PrismaClient({ adapter, log: ['query']});
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
